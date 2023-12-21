7f97453fbf481991d7c42de9179907130ae9d7d8
Ypriom121 <pritomgupta121@gmail.com>
2023-12-21 10:45:28 +0600

    initital set up project

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Main = () => {
     return (
         <>
+          <Navbar/>
             <Header />
             <Outlet />
             <Footer />
